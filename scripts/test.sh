e929f611ed53885dd3d54b7a126058ba470896f8
x1a0qiya <mn124356@163.com>
2026-07-17 17:05:52 +0800

    <fix>:更新文档,脚本

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /Ommateum
-
-python ./src/ommateum/models/test.py \
-    --model_path ./weights/yolo/trained/train01_best.pt \
-    --source ./dataset/Tumor-Detection/test/images \
-    --device cuda \
-    --project ./dataset/Tumor-Detection/ts1/ \
